7b901c1e17170212d043d964ca60a4f4e0cd04b1
sve009 <sve9milo@gmail.com>
2022-05-15 17:02:27 -0500

    Worked some more lol. Still doesn't work

--- kernel/debug.c
+++ kernel/debug.c
@@ -2,6 +2,7 @@
 
 #include "kprint.h"
 #include "elf.h"
+#include "paging.h"
 
 // #include <mystring.h>
 
@@ -16,6 +17,9 @@ int num_symbols;
 
 // Find + set tables
 void init_tables(struct stivale2_struct_tag_modules* tag) {
+  // Sanity check
+  Elf64_Shdr* temp; 
+
   for (int i = 0; i < tag->module_count; i++) {
     // Find kernel mod
     if (my_strcmp(tag->modules[i].string, "kernel") != 0) {
@@ -28,13 +32,37 @@ void init_tables(struct stivale2_struct_tag_modules* tag) {
     // Get section table
     Elf64_Shdr* shdr = (Elf64_Shdr*)((uint64_t)ehdr->e_shoff + (uint64_t)ehdr);
 
-    // Set number of symbols
-    num_symbols = shdr[2].sh_size / (sizeof(Elf64_Sym));
+    // Loop until both symbol + string tables found
 
-    // Set strtab and symtab
-    symtab = (Elf64_Sym*)((uint64_t)ehdr + (uint64_t)shdr[2].sh_offset);
-    strtab = (char*)((uint64_t)ehdr + (uint64_t)shdr[3].sh_offset);
+    while (strtab == NULL || symtab == NULL) {
+
+      // Symbol table
+      if (shdr->sh_type == 2) {
+        // Set number of symbols
+        //num_symbols = shdr->sh_size / (sizeof(Elf64_Sym));
+        num_symbols = shdr->sh_size / shdr->sh_entsize;
+
+        kprintf("%d Symbols found\n", num_symbols);
+
+        // Set symtab
+        symtab = (Elf64_Sym*)(get_hhdm() + (uint64_t)ehdr + (uint64_t)shdr->sh_offset);
+        temp = shdr;
+      }
+
+      // String table
+      if (shdr->sh_type == 3) {
+        strtab = (char*)(get_hhdm() + (uint64_t)ehdr + (uint64_t)shdr->sh_offset);
+      }
+
+      // Increment header entry
+      shdr++;
+    }
   }
+  /** kprintf("Hello\n"); */
+  /** kprintf("strtab: %p\n", strtab); */
+
+  // Debug make sure string table works
+  /** kprintf("Name: %s\n", &strtab[15]); */
 }
 
 // Lookup symbol in table
@@ -46,6 +74,7 @@ uint64_t lookup_symbol(char* symbol) {
   //      OR return address
   for (int i = 0; i < num_symbols; i++) {
     char* sym_string = &strtab[symtab[i].st_name];
+    kprintf("%s\n", sym_string);
     if (my_strcmp(symbol, sym_string == 0)) {
       return symtab[i].st_value;
     }
